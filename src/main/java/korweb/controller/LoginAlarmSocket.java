dafc0bd2f30e4c6f27fe2bf1f342333f8d6f1e68
Yim_JunSu <yimjunsu@gmail.com>
2025-02-23 23:42:18 +0900

    Web Service Study

--- src/main/java/korweb/controller/LoginAlarmSocket.java
+++ src/main/java/korweb/controller/LoginAlarmSocket.java
@@ -17,17 +17,20 @@ public class LoginAlarmSocket extends TextWebSocketHandler{
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws  Exception{
+        // 클라이언트 소켓 서버 소켓 연동 성공시 리스트 담기
         System.out.println("클라이언트 서버에 로그인 접속 성공!!");
         loginlist.add(session);
     }
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws  Exception{
+        // 클라이언트 소켓 서버 소켓 연동 끊을시 리스트 꺼내기
         System.out.println("클라이언트와 접속이 끊겼다.");
         loginlist.remove(session);
     }
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message )throws Exception{
         System.out.println("로그인 알림 받음 : " + message.getPayload());
+        // [3] 서버소켓이 특정 클라이언트 소켓으로 부터 받은 메시지를 접속된 모든 클라이언트 소켓들에게 보내기
         for(int index = 0; index<=loginlist.size()-1;index++){
         WebSocketSession inclient = loginlist.get(index);
         inclient.sendMessage(message);
